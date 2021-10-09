bab0778b98298162d80bb464e04ad01761927dc2
davi-souza2001 <davisouza2001dv@gmail.com>
2021-10-09 11:04:18 -0300

    add view form

--- src/views/UseForm.js
+++ src/views/UseForm.js
@@ -1,8 +1,49 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 
-export default function UserForm() {
+export default function UserForm({route, navigation}) {
+   const [user, setUser] = useState(route.params ? route.params : {});
     return (
-        <Text>UserForm</Text>
+        <View style={style.form}>
+            <Text>Nome</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={name => setUser({...user, name})}
+                placeholder="Informe o Nome"
+                value={user.name}
+                />
+            <Text>Email</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={email => setUser({...user, email})}
+                placeholder="Informe o E-mail"
+                value={user.email}
+                />
+            <Text>URL do Avatar</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+                placeholder="Informe a URL do Avatar"
+                value={user.avatarUrl}
+                />
+            <Button
+                title="Salvar"
+                onPress={() => {
+                    navigation.goBack();
+                }}
+            />
+        </View>
     )
 }
+
+const style = StyleSheet.create({
+    form: {
+        padding: 12
+    },
+    input: {
+        height: 40,
+        borderColor:  "gray",
+        borderWidth: 1,
+        marginBottom: 10,
+    }
+})
