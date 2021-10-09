a51575189f4bdeb7e7c8b2b83b8bd3e02e8006b6
davi-souza2001 <davisouza2001dv@gmail.com>
2021-10-09 09:50:30 -0300

    add array list

---
+++ src/data/users.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        name: "João Silva",
+        email: "joao@empmail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449__340.png"
+    },
+    {
+        id: 2,
+        name: "Mariana Aguiar",
+        email: "Mariana@empmail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375__340.png"
+    },
+    {
+        id: 3,
+        name: "Julio Fragoso",
+        email: "julio@empmail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773__340.png"
+    },
+    {
+        id: 4,
+        name: "Rafael Monteiro",
+        email: "rafael@empmail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635448__340.png"
+    },
+    {
+        id: 5,
+        name: "Davi Souza",
+        email: "davi@empmail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635464__340.png"
+    },
+    {
+        id: 6,
+        name: "Joel Monteiro",
+        email: "joel@empmail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582__340.png"
+    }
+]
